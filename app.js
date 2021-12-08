3b3b803fe4d81ac0955685186ce4ca144a91b3e2
Maksym Klymenko <qklymenko.maksym@gmail.com>
2021-12-08 22:03:03 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const slides = document.querySelectorAll('.slide')
+
+function clearActiveClasses() {
+    slides.forEach((slide) =>{
+        slide.classList.remove('active')
+    })
+}
+
+for (const slide of slides) {
+    slide.addEventListener('click', () => {
+        clearActiveClasses();
+
+        slide.classList.add('active')
+    })
+}
